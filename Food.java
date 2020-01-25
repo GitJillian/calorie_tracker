a2b8de8ec56240325873096f1331e61029552fd3
Ruinstorm <60301108+Ruinstorm@users.noreply.github.com>
2020-01-25 16:19:25 -0500

    Create Food.java

---
+++ Food.java
@@ -0,0 +1,21 @@
+public class Food{
+  
+  private String name;
+  private Float calorie;
+  private Boolean status;
+
+  public Food(String name, Float calorie, Boolean status){
+    this.name = name;
+    this.calorie = calorie;
+    this.status = status;
+  }
+  
+  public void SetFood(String name, Float calorie, Boolean status){
+    this.name = name;
+    this.calorie = calorie;
+    this.status = status;
+  }
+  
+  
+
+}
