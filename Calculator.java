00772f0368445af8777c7e587b9a31ff163804de
Ruinstorm <60301108+Ruinstorm@users.noreply.github.com>
2020-02-15 14:43:46 -0500

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -3,7 +3,14 @@ public class Calculator{
   private Float sum;
   private Boolean isWarning;
   
-
-
-
+  public Calculator(Float sum){
+    this.sum = sum;
+  }
+  
+  public Float CalorieSum(Float f){
+  
+  
+    
+  }
+  
 }
